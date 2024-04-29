73f0ff760860bdf780c58fd496bd814fce9cc790
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-29 22:27:54 +0000

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Flask app"""
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,12 +11,18 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(exception):
     """Close database session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     POST = getenv('HBNB_API_PORT', 5000)
-    app.run(host=HOST, port=POST, threaded=True)
+    app.run(debug=True, host=HOST, port=POST, threaded=True)
