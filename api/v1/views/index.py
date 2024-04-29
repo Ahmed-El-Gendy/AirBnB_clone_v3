4c9cc2ba71d295ac6a7c105887e72e3a6fbee799
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-29 22:15:26 +0000

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,17 @@
 """Index view"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Return status"""
-    return jsonify({"status": "OK"})
+    return jsonify({
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    })
