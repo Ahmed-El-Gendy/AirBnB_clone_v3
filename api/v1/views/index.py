41cb4ca077e5979f44a29ceaf85a387839ce1fc1
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-29 22:31:24 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,11 +8,4 @@ from models import storage
 @app_views.route('/status')
 def status():
     """Return status"""
-    return jsonify({
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
-    })
+    return jsonify({"status": "OK"})
