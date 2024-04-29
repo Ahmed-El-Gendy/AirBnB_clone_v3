c79b1b4c125bde5d049275262b7fc2985d636ea3
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-04-29 22:07:03 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_db(error):
     """Close database session"""
     storage.close()
+
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
