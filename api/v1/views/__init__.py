efedff698f655e1b3bf4274feb8c65f974446423
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-05-01 21:53:30 +0000

    task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,6 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # wildcard import
 from api.v1.views.index import *
-if app_views is not None:
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
